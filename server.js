e4b89edc1ff81107938e1e6441042cb23995b538
Your Name <you@example.com>
2021-10-09 19:30:25 +0200

    sa

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
 
+//***
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
+
 var app = express();
 
 app.use(cors());
@@ -11,7 +15,9 @@ app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.post('/api/fileanalyse',upload.any(),function(req,res){
+  res.send({name: req.files[0].originalname, type: req.files[0].mimetype, size: req.files[0].size});
+});
 
 
 const port = process.env.PORT || 3000;
